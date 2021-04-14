b2568936d2f5e55a18f13d33a181d8d8c0164847
celaraze <celaraze@qq.com>
2021-04-14 14:27:15 +0800

    框架：初始化

---
+++ src/Response.php
@@ -0,0 +1,38 @@
+<?php
+
+
+namespace Response;
+
+
+use Exception;
+use Illuminate\Http\JsonResponse;
+
+class Response
+{
+    /**
+     * 通用的接口返回构造数组
+     * @param int $code
+     * @param string $message
+     * @param array $data
+     * @param bool $array
+     * @return array|JsonResponse
+     */
+    public static function make(int $code, string $message, array $data = [], bool $array = false): JsonResponse|array
+    {
+        $return = [];
+        $return['code'] = $code;
+        if ($data instanceof Exception) {
+            $return['code'] = 500;
+            $message = $data->getLine() . ' : ' . $data->getMessage();
+            $data = [];
+        }
+        $return['message'] = $message;
+        $return['data'] = $data;
+
+        if ($array) {
+            return $return;
+        }
+
+        return response()->json($return);
+    }
+}
