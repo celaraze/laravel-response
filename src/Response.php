7f60458e495a3120867045fb385ffdee8f8ea4b6
celaraze <celaraze@qq.com>
2021-04-14 15:35:03 +0800

    优化：添加注解

--- src/Response.php
+++ src/Response.php
@@ -6,17 +6,19 @@ namespace Response;
 
 use Exception;
 use Illuminate\Http\JsonResponse;
+use JetBrains\PhpStorm\ArrayShape;
 
 class Response
 {
     /**
-     * 通用的接口返回构造数组
+     * 通用的接口返回构造数组.
      * @param int $code
      * @param string $message
      * @param array $data
      * @param bool $array
      * @return array|JsonResponse
      */
+    #[ArrayShape(['code' => "integer", 'message' => "string", "data" => "mixed"])]
     public static function make(int $code, string $message, array $data = [], bool $array = false): JsonResponse|array
     {
         $return = [];
